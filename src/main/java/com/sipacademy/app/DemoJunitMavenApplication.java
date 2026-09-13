2c8829fb2cfcbd21390833ddcef1d7433cef124d
Mohamed Amine MEZGHICH <git@mezghich.com>
2026-09-13 12:31:25 +0100

    first commit

---
+++ src/main/java/com/sipacademy/app/DemoJunitMavenApplication.java
@@ -0,0 +1,13 @@
+package com.sipacademy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJunitMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJunitMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sipacademy/app/DemoJunitMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sipacademy.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJunitMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
